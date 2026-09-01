5124976b2e1a8b6dd1476d0d5c63d3244f781395
BJH028-H <bagjeonghyeog028@gmail.com>
2026-09-01 20:12:18 +0900

    sava

--- SW-AI-W02-03-TEMPLATE-master/week2/2. advanced/04_hanoi_tower.py
+++ SW-AI-W02-03-TEMPLATE-master/week2/2. advanced/04_hanoi_tower.py
@@ -55,12 +55,20 @@
         결과 리스트에 (src, dst) 추가   # 가장 큰 원반을 dst 로
         move(k-1, via, src, dst)        # via 의 k-1개를 dst 로
 """
-
+def move(n, src, via, dst):
+    if n == 0: return []
+    moves = []
+    moves.extend(move(n-1, src, dst, via))
+    moves.append((src, dst))
+    moves.extend(move(n-1, via, src, dst))
+    return moves
 
 def hanoi_count(n: int) -> int:
     """N 개의 원반을 옮기는 데 필요한 최소 이동 횟수( = 2^N - 1) 를 반환"""
     # TODO: 2^N - 1 을 정수로 반환하세요.
-    pass
+    return 2 ** n - 1
+
+
 
 
 def hanoi_moves(n: int) -> list:
@@ -73,8 +81,9 @@ def hanoi_moves(n: int) -> list:
     """
     # TODO: N > 20 또는 N == 0 인 경우 [] 를 반환하세요.
     # TODO: 그 외에는 재귀로 이동 순서를 만들어 반환하세요.
-    pass
-
+    if n == 0 or n > 20:
+        return []
+    return move(n, 1, 2, 3)
 
 if __name__ == "__main__":
     print("[테스트 1] N=0 (원반 없음, 옮길 것 없음)")
